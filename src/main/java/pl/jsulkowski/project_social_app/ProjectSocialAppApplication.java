ed5d014b10a32ca7132f628b0f8a33a8fa42e248
Nidan23 <dev.sulkowski@gmail.com>
2022-05-01 15:43:30 +0200

    Initial Commit

---
+++ src/main/java/pl/jsulkowski/project_social_app/ProjectSocialAppApplication.java
@@ -0,0 +1,13 @@
+package pl.jsulkowski.project_social_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSocialAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSocialAppApplication.class, args);
+    }
+
+}
